138b6560cebe3b21d06b73c90a5680e452274662
Rebecca <rrossman25@gmail.com>
2019-05-04 17:31:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value){
   return object;
 }
 
-function updateObjectWithKeyAndValue(object, key, value{
+// function updateObjectWithKeyAndValue(object, key, value{
   
-}
+// }
