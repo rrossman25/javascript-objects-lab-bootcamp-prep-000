41097d4a920b019e879faff8293694542474d2fc
Rebecca <rrossman25@gmail.com>
2019-05-04 17:31:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let recipes = {eggs:2; salt: '1 tsp'; butter:'8 cups'};
+let recipes = {eggs:2, salt: '1 tsp', butter:'8 cups'};
 
 function updateObjectWithKeyAndValue(object, key, value){
   object[key] =value;
