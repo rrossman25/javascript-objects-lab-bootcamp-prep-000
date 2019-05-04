58ee14e3cdb9f3a9b7431dbe8adeca2bbad96658
Rebecca <rrossman25@gmail.com>
2019-05-04 17:34:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let recipes = {eggs:2, salt: '1 tsp', butter:'8 cups'};
 
-function updateObjectWithKeyAndValue(object, key, value){
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] =value;
   return object;
 }
 
 function updateObjectWithKeyAndValue(object, key, value{
-  
+  let obj = {};
+  object.assign(obj, object);
+  obj[key] =value;
+  return obj;
 }
