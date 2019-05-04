a444cf174a7d540cb13b74df412154bd78383a52
Rebecca <rrossman25@gmail.com>
2019-05-04 17:35:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object;
 }
 
-function updateObjectWithKeyAndValue(object, key, value{
+function updateObjectWithKeyAndValue(object, key, value){
   let obj = {};
   object.assign(obj, object);
   obj[key] =value;
